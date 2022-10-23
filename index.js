03e534c2d6b683128b21d2b5507d307a68c37332
labibahmed10 <labib.ahmed.372@gmail.com>
2022-10-23 23:37:18 +0600

    Worked again almost every route and tried to fix any kind of bugs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const dotenv = require("dotenv");
 
 //routes
 const userRoute = require("./Routes/user.route");
@@ -13,9 +14,7 @@ const hiringManagerRoute = require("./Routes/hiringManager.route");
 // middlewares
 app.use(cors());
 app.use(express.json());
-
-// database connection here
-require("./server");
+dotenv.config();
 
 // declaration of routes
 app.use("/api/", generalRoute);
@@ -24,13 +23,16 @@ app.use("/api/admin", adminRoute);
 app.use("/api/", candidateRoute);
 app.use("/api/", hiringManagerRoute);
 
-app.get("/", (req, res) => {
-   res.send("Welcome to the job management system api");
-});
+// database connection here
+require("./server");
 
 // for invalid route will fix later
 app.use("*", (req, res) => {
-   res.send("Go to the exact location");
+   res.send("Go to the exact location like - /api/user");
+});
+
+app.get("/", (req, res) => {
+   res.send("Welcome to the job management system api");
 });
 
 app.listen(port, () => {
