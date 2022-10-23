6405fc3ac8a293d900c3255e59617d4f71f4a928
labibahmed10 <labib.ahmed.372@gmail.com>
2022-10-23 23:52:38 +0600

    Worked again almost every route and tried to fix any kind of bugs

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 const { findUserByEmail } = require("../Service/user.service");
 
-
 exports.verifyToken = async (req, res, next) => {
    const token = req.headers?.authorization?.split(" ")[1];
    if (!token) {
@@ -13,7 +12,7 @@ exports.verifyToken = async (req, res, next) => {
    }
 
    try {
-      const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+      const decode = await promisify(jwt.verify)(token, process.env.TOKEN_SECRET);
       const user = await findUserByEmail(decode.email);
       req.user = user;
       next();
